3b5a6e61a607ef01616fc92a1a9e354acd72aa6f
Omer Gilad Shiloh <omer_psi@webiks.com>
2021-05-03 14:44:10 +0300

    New

---
+++ api/cat.py
@@ -0,0 +1,14 @@
+from http.server import BaseHTTPRequestHandler
+from datetime import datetime
+from random import randint
+
+with open('../pictures/cat.txt') as pictures:
+pics = pictures.read().splitlines()
+
+class handler(BaseHTTPRequestHandler):
+  def do_GET(self):
+    self.send_response(200)
+    self.send_header('Content-type', 'text/html')
+    self.end_headers()
+    self.wfile.write('<img src="' + pics[randint(0, len(pics) - 1)] + '"/>'.encode())
+    return
